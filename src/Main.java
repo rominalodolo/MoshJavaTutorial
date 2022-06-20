15cecdcbcd1150f8314176c14906ac449f24f484
Romina <lodoloromina@gmail.com>
2022-06-20 12:29:05 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -210,7 +210,13 @@ public class Main {
 
 
 // Switch Statements
-
+        String role = "admin";
+        if (role == "admin")
+            System.out.println("You're an admin");
+        else if (role == "moderator")
+            System.out.println("You're a moderator");
+        else
+            System.out.println("You're a guest");
 
 
 // Conditional Statements
